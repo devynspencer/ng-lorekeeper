45593dfb8c143c1f7d9f37b72005fe625eb0217c
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-06 23:32:07 -0700

    Chore - lint & format

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-users',
-  template: `
-    <p>
-      users works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-users",
+  template: ` <p>users works!</p> `,
+  styles: [],
 })
 export class UsersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
