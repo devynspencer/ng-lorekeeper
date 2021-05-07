d973c4238b6609b9f604e7d92c499088f0feb842
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-06 23:03:21 -0700

    Prefer mapped paths for imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "@app/app-routing.module";
+import { AppComponent } from "@app/app.component";
 import { StoreModule } from "@ngrx/store";
-import { reducers, metaReducers } from "./reducers";
+import { reducers, metaReducers } from "@app/reducers";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { environment } from "../environments/environment";
+import { environment } from "@env/environment";
 
 @NgModule({
   declarations: [AppComponent],
