6f6f2e44d1292dd6fd7877d0beaf6d7c9efbde8b
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-06 23:04:54 -0700

    Standardize module import order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
+import { environment } from "@env/environment";
 import { AppRoutingModule } from "@app/app-routing.module";
 import { AppComponent } from "@app/app.component";
-import { StoreModule } from "@ngrx/store";
 import { reducers, metaReducers } from "@app/reducers";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { environment } from "@env/environment";
 
 @NgModule({
   declarations: [AppComponent],
